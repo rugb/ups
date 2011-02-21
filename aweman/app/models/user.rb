e2c77a54b2f29c05976ba626c8b999543ee35efd
Dennis Boerm <allspark@planetcyborg.de>
2011-02-21 14:06:12 +0100

    user model

--- aweman/app/models/user.rb
+++ aweman/app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ActiveRecord::Base
+  attr_accessible :name, :email
+  
+  email_regex = /\A[\w+\-.]{1,128}@[a-z\d\-.]{1,128}\.[a-z]{2,4}\z/i
+  
+  validates :name, :presence => true, :length => { :maximum => 50 }
+  validates :email, :presence => true, :format => { :with => email_regex }, :uniqueness => { :case_sensitive => false }
+  
+  
 end
