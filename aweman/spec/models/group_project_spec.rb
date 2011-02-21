dccf9d7bc20d0b85eec872e3d5dd94984ee7345d
Dennis Boerm <allspark@planetcyborg.de>
2011-02-21 15:13:12 +0100

    groupproject model/test

--- aweman/spec/models/group_project_spec.rb
+++ aweman/spec/models/group_project_spec.rb
@@ -1,5 +1,36 @@
 require 'spec_helper'
 
 describe GroupProject do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @group = Group.new(:nr => 23)
+    @project = Project.new(:name => "foo")
+
+    @groupproject = @group.groupprojects.build(:project_id => @project.id)
+  end
+
+  it "should create a new instance given valid attributes" do
+    @groupproject.save!
+  end
+
+  describe "group/project methods" do
+    before(:each) do
+      @groupproject.save
+    end
+    
+    it "should have a group attribute" do
+      @groupproject.should respond_to(:group)
+    end
+    
+    it "should have the right group" do
+      @groupproject.group.should == @group
+    end
+    
+    it "should have a project attribute" do
+      @groupproject.should respond_to(:project)
+    end
+    
+    it "should have the right project" do
+      @groupproject.project.should == @project
+    end
+  end
 end
