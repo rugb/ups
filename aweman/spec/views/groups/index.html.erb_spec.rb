c4c9b3d13dcc6f1fa253af223b862d404c8c7e24
Dennis Boerm <allspark@planetcyborg.de>
2011-02-21 13:31:44 +0100

    group model

---
+++ aweman/spec/views/groups/index.html.erb_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe "groups/index.html.erb" do
+  before(:each) do
+    assign(:groups, [
+      stub_model(Group,
+        :nr => 1
+      ),
+      stub_model(Group,
+        :nr => 1
+      )
+    ])
+  end
+
+  it "renders a list of groups" do
+    render
+    # Run the generator again with the --webrat flag if you want to use webrat matchers
+    assert_select "tr>td", :text => 1.to_s, :count => 2
+  end
+end
