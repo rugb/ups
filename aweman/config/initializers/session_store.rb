05012be51e09b8f745a3982cf3ab31c7e3bd4b8d
Philip Häusler <msquare@notrademark.de>
2011-02-21 12:58:11 +0100

    new rails project

---
+++ aweman/config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Aweman::Application.config.session_store :cookie_store, :key => '_aweman_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Aweman::Application.config.session_store :active_record_store
