c49542fe996405421706df31bdfd558d40bb1075
Dennis Boerm <allspark@planetcyborg.de>
2011-02-21 15:12:39 +0100

    group model

--- aweman/app/models/group.rb
+++ aweman/app/models/group.rb
@@ -3,4 +3,7 @@ class Group < ActiveRecord::Base
   
   validates :nr, :uniqueness => true
   validates_numericality_of :nr, :greater_than => 0, :only_integer => true, :message => "must be greater than 0"
+  
+  has_many :group_projects, :dependent => :destroy
+  has_many :projects, :through => :group_projects
 end
