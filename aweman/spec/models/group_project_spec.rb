98747637002a85227a581d86d1e699cd65f34c02
Philip Häusler <msquare@notrademark.de>
2011-02-21 15:38:05 +0100

    fixed group project test

--- aweman/spec/models/group_project_spec.rb
+++ aweman/spec/models/group_project_spec.rb
@@ -4,47 +4,49 @@ describe GroupProject do
   before(:each) do
     @group = Group.new(:nr => 23)
     @project = Project.new(:name => "foo")
-
-    @groupproject = @group.group_projects.build(:project_id => @project.id)
+    @group.save
+    @project.save
+    
+    @group_project = @group.group_projects.build(:project_id => @project.id)
   end
-
+  
   it "should create a new instance given valid attributes" do
-    @groupproject.save!
+    @group_project.save
   end
-
+  
   describe "group/project methods" do
     before(:each) do
-      @groupproject.save
+      @group_project.save
     end
     
     it "should have a group attribute" do
-      @groupproject.should respond_to(:group)
+      @group_project.should respond_to(:group)
     end
     
     it "should have the right group" do
-      @groupproject.group.should == @group
+      @group_project.group.should == @group
     end
     
     it "should have a project attribute" do
-      @groupproject.should respond_to(:project)
+      @group_project.should respond_to(:project)
     end
     
     it "should have the right project" do
-      @groupproject.project.should == @project
+      @group_project.project.should == @project
     end
   end
   
   describe "validations" do
-
+    
     it "should require a project_id" do
-      @groupproject.project_id = nil
-      @groupproject.should_not be_valid
+      @group_project.project_id = nil
+      @group_project.should_not be_valid
     end
-
+    
     it "should require a group_id" do
-      @groupproject.group_id = nil
-      @groupproject.should_not be_valid
+      @group_project.group_id = nil
+      @group_project.should_not be_valid
     end
   end
-
+  
 end
