dc164dace44d28fd035f4608478b0a6f92291835
Dennis Boerm <allspark@planetcyborg.de>
2011-02-21 14:32:59 +0100

    project test and model

--- aweman/app/models/project.rb
+++ aweman/app/models/project.rb
@@ -1,2 +1,5 @@
 class Project < ActiveRecord::Base
+  attr_accessible :name, :description
+  
+  validates :name, :presence => true, :length => { :maximum => 50 }, :uniqueness => true
 end
--- aweman/spec/models/project_spec.rb
+++ aweman/spec/models/project_spec.rb
@@ -1,5 +1,31 @@
 require 'spec_helper'
 
 describe Project do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @attr = {
+      :name => "test project",
+      :description => "foobar"
+      }
+  end
+  
+  it "should create a new instance given valid attrs" do
+    Project.create!(@attr)
+  end
+  
+  it "should have a not empty name" do
+      no_name_user = Project.create(@attr.merge(:name => ""))
+      no_name_user.should_not be_valid
+  end
+  
+  it "should reject names that are too long" do
+      long_name = "a" * 51
+      long_name_project = Project.new(@attr.merge(:name => long_name))
+      long_name_project.should_not be_valid
+  end
+  
+  it "should reject duplicate names" do
+      Project.create!(@attr)
+      duplicate_project = Project.new(@attr)
+      duplicate_project.should_not be_valid
+  end
 end
