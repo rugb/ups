38e9703f6ef3fe06fbe77c76ac7b5b75c69043c9
Dennis Boerm <allspark@planetcyborg.de>
2011-02-21 14:37:05 +0100

    db schema

--- aweman/db/schema.rb
+++ aweman/db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110221123841) do
+ActiveRecord::Schema.define(:version => 20110221133556) do
 
   create_table "clients", :force => true do |t|
     t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "group_projects", :force => true do |t|
+    t.integer  "group_id"
+    t.integer  "project_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
