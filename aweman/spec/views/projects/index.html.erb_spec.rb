e498125a169122ef685f79523be27ef503312da8
Dennis Boerm <allspark@planetcyborg.de>
2011-02-22 10:54:38 +0100

    fix project index spec

--- aweman/spec/views/projects/index.html.erb_spec.rb
+++ aweman/spec/views/projects/index.html.erb_spec.rb
@@ -18,11 +18,8 @@ describe "projects/index.html.erb" do
 
   it "renders a list of projects" do
     render
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
+
     assert_select "tr>td", :text => "Name".to_s, :count => 2
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => 1.to_s, :count => 2
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "tr>td", :text => "Description".to_s, :count => 2
   end
 end
