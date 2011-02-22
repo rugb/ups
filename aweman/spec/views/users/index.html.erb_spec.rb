2e61d25fbcd2048e342fb9683bc12a4274d0ef74
Philip Häusler <msquare@notrademark.de>
2011-02-22 09:51:14 +0100

    views users index spec

--- aweman/spec/views/users/index.html.erb_spec.rb
+++ aweman/spec/views/users/index.html.erb_spec.rb
@@ -3,26 +3,22 @@ require 'spec_helper'
 describe "users/index.html.erb" do
   before(:each) do
     assign(:users, [
-      stub_model(User,
+    stub_model(User,
         :name => "Name",
         :email => "Email",
-        :group_id => 1
-      ),
-      stub_model(User,
+        :group_id => nil
+    ),
+    stub_model(User,
         :name => "Name",
         :email => "Email",
-        :group_id => 1
-      )
+        :group_id => nil
+    )
     ])
   end
-
+  
   it "renders a list of users" do
     render
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => "Name".to_s, :count => 2
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => "Email".to_s, :count => 2
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => 1.to_s, :count => 2
+    assert_select "tr>td", :text => "Name", :count => 2
+    assert_select "tr>td", :text => "Email", :count => 2
   end
 end
