755aed2e7e73fdbedaaf31ccc003ad2c390fe089
Philip Häusler <msquare@notrademark.de>
2011-02-22 10:13:29 +0100

    helpers user spec

--- aweman/spec/helpers/users_helper_spec.rb
+++ aweman/spec/helpers/users_helper_spec.rb
@@ -1,13 +1,12 @@
 require 'spec_helper'
 
 describe UsersHelper do
-  
-  before(:each) do
-    @user = User.create!(:name => "Bill Gates", :email => "gates@microsoft.com")
-    @user2 = User.new(:name => "Hans Meier", :email => "hans@meier.de")
-  end
-  
   describe "free users options" do
+    before(:each) do
+      @user = User.create!(:name => "Bill Gates", :email => "gates@microsoft.com")
+      @user2 = User.new(:name => "Hans Meier", :email => "hans@meier.de")
+    end
+    
     it "should be empty if only one user exists" do
       free_users_options(@user).should == []
     end
@@ -15,6 +14,21 @@ describe UsersHelper do
     it "should contain the other users name and id if two users exists" do
       @user2.save
       free_users_options(@user).should == [[@user2.name, @user2.id]]
+    end
+  end
+  
+  describe "groups options" do
+    before(:each) do
+      @group = Group.new(:nr => 23)
+    end
+    
+    it "should be empty if no group exists" do
+      groups_options.should == []
+    end
+    
+    it "should contain one groups number and id if one group exists" do
+      @group.save
+      groups_options.should == [[@group.nr, @group.id]]
     end
   end
 end
