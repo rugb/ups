616684d061b62afeb18d063ac976eff99e001470
Philip Häusler <msquare@notrademark.de>
2011-02-22 18:10:40 +0100

    model page validation

--- ups/app/models/page.rb
+++ ups/app/models/page.rb
@@ -1,3 +1,7 @@
 class Page < ActiveRecord::Base
   has_many :page_contents
+  
+  validate do |record|
+    record.errors.add :parent_id, "cannot have itself as parent" if !record.parent_id.nil? && record.parent_id == record.id
+  end
 end
