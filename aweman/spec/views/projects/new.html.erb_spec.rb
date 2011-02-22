4241d29200aeea989688430b8f4ffa58dc009c73
Dennis Boerm <allspark@planetcyborg.de>
2011-02-22 10:53:12 +0100

    projects new spec

--- aweman/spec/views/projects/new.html.erb_spec.rb
+++ aweman/spec/views/projects/new.html.erb_spec.rb
@@ -15,7 +15,7 @@ describe "projects/new.html.erb" do
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "form", :action => projects_path, :method => "post" do
       assert_select "input#project_name", :name => "project[name]"
-      assert_select "input#project_client_id", :name => "project[client_id]"
+      assert_select "select#project_client_id", :name => "project[client_id]"
       assert_select "input#project_description", :name => "project[description]"
     end
   end
