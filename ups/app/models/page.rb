2579915afad053aaf0835bc83a0370bbd0698f78
Philip Häusler <msquare@notrademark.de>
2011-02-22 18:16:48 +0100

    model page validation test fix

--- ups/app/models/page.rb
+++ ups/app/models/page.rb
@@ -1,8 +1,11 @@
 class Page < ActiveRecord::Base
   has_many :page_contents
   
+  # reject pages having itself as parent
   validate do |record|
     record.errors.add :parent_id, "cannot have itself as parent" if !record.parent_id.nil? && record.parent_id == record.id
   end
   validates_numericality_of :position, :only_integer => true, :greater_than => 0, :allow_nil => 0
+  validates_presence_of :type
+  
 end
