1ee06676ed3f2bc17b4159cd5831c5487ce3c94c
Philip Häusler <msquare@notrademark.de>
2011-02-22 20:04:53 +0100

    configuration model

--- ups/spec/models/page_content_spec.rb
+++ ups/spec/models/page_content_spec.rb
@@ -7,13 +7,13 @@ describe PageContent do
       :text => "page text",
       :excerpt => "page",
       :language_id => 1
-      }
+    }
     
     @attr_page = {
       :page_type => :page,
       :enabled => true,
       :int_title => "test_page"
-      }
+    }
     @page = Page.create!(@attr_page)
   end
   
@@ -25,62 +25,62 @@ describe PageContent do
     
     describe "title" do
       it "should not accept a NIL title" do
-	PageContent.new(@attr.merge(:title => NIL)).should_not be_valid
+        PageContent.new(@attr.merge(:title => NIL)).should_not be_valid
       end
       
       it "should not accept an empty title" do
-	PageContent.new(@attr.merge(:title => "")).should_not be_valid
+        PageContent.new(@attr.merge(:title => "")).should_not be_valid
       end
       
       it "should reject title that are too long" do
-	long_title = "a" * 51
-	PageContent.new(@attr.merge(:title => long_title)).should_not be_valid
+        long_title = "a" * 51
+        PageContent.new(@attr.merge(:title => long_title)).should_not be_valid
       end
     end
     
     describe "page_id" do
       
       it "should not accept a NIL page_id" do
-	PageContent.new(@attr.merge(:page_id => NIL)).should_not be_valid
+        PageContent.new(@attr.merge(:page_id => NIL)).should_not be_valid
       end
-    
+      
       it "should not accept a page_id equal/less 0" do
-	PageContent.new(@attr.merge(:page_id => 0)).should_not be_valid
+        PageContent.new(@attr.merge(:page_id => 0)).should_not be_valid
       end
       
       describe "relation" do
-	
-	it "should have the right page_id" do
-	  pageContent = @page.page_contents.build(@attr)
-	  pageContent.page.int_title.should == @attr_page[:int_title]
-	end
+        
+        it "should have the right page_id" do
+          pageContent = @page.page_contents.build(@attr)
+          pageContent.page.int_title.should == @attr_page[:int_title]
+        end
       end
     end
     
     describe "language" do
       
       it "should not accept an empty language" do
-	PageContent.new(@attr.merge(:language_id => NIL)).should_not be_valid
+        PageContent.new(@attr.merge(:language_id => NIL)).should_not be_valid
       end
-    
+      
       it "should not have a language_id equal 0" do
-	PageContent.new(@attr.merge(:lanuage_id => 0)).should_not be_valid
+        PageContent.new(@attr.merge(:lanuage_id => 0)).should_not be_valid
       end
       
       describe "relation" do
-	before(:each) do
-	  @lang_attr = {
+        before(:each) do
+          @lang_attr = {
 	    :short => "de",
 	    :name => "deutsch"
-	  }
-	  @lang = Language.create!(@lang_attr)
-	end
-	
-	it "should have the right language" do
-	  page_content = @page.page_contents.build(@attr.merge(:language_id => @lang.id))
-	  
-	  page_content.language.short.should == @lang_attr[:short]
-	end
+          }
+          @lang = Language.create!(@lang_attr)
+        end
+        
+        it "should have the right language" do
+          page_content = @page.page_contents.build(@attr.merge(:language_id => @lang.id))
+          
+          page_content.language.short.should == @lang_attr[:short]
+        end
       end
     end
   end
