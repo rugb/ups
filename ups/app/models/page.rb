14f22f1aded7e41450b2dec900ea22d066c9b225
Philip Häusler <msquare@notrademark.de>
2011-02-22 18:13:38 +0100

    model page validation

--- ups/app/models/page.rb
+++ ups/app/models/page.rb
@@ -4,4 +4,5 @@ class Page < ActiveRecord::Base
   validate do |record|
     record.errors.add :parent_id, "cannot have itself as parent" if !record.parent_id.nil? && record.parent_id == record.id
   end
+  validates_numericality_of :position, :only_integer => true, :greater_than => 0, :allow_nil => 0
 end
