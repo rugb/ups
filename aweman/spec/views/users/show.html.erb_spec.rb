be38b0633c3fc62b2e1c11b90f946a94ce6c269c
Philip Häusler <msquare@notrademark.de>
2011-02-22 10:02:19 +0100

    views users show spec

--- aweman/spec/views/users/show.html.erb_spec.rb
+++ aweman/spec/views/users/show.html.erb_spec.rb
@@ -1,21 +1,37 @@
 require 'spec_helper'
 
 describe "users/show.html.erb" do
-  before(:each) do
-    @user = assign(:user, stub_model(User,
-      :name => "Name",
-      :email => "Email",
-      :group_id => 1
-    ))
+  describe "user without group" do
+    before(:each) do
+      @user = User.create!(:name => "Bill Gates", :email => "gates@microsoft.com")
+    end
+    
+    it "renders attributes in <p> and group building form" do
+      render
+      
+      rendered.should match(/Name/)
+      rendered.should match(/Email/)
+      
+      assert_select "form", :action => group_with_user_path(@user), :method => "post" do
+        assert_select "select#group_with_user_id", :name => "group_with[group_id]"
+      end
+    end
   end
-
-  it "renders attributes in <p>" do
-    render
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    rendered.should match(/Name/)
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    rendered.should match(/Email/)
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    rendered.should match(/1/)
+  
+  describe "user with group" do
+    before(:each) do
+      @user = User.create!(:name => "Bill Gates", :email => "gates@microsoft.com")
+      @group = Group.create!(:nr => 23)
+      @user.group = @group
+      @user.save
+    end
+    
+    it "renders attributes in <p>" do
+      render
+      
+      rendered.should match(/Name/)
+      rendered.should match(/Email/)
+      rendered.should match(/23/)
+    end
   end
 end
