44c74f0f649547ccbe464fa322cfb24ab62f0a8f
Philip Häusler <msquare@notrademark.de>
2011-02-22 17:32:37 +0100

    model page spec

--- ups/spec/models/page_spec.rb
+++ ups/spec/models/page_spec.rb
@@ -30,18 +30,36 @@ describe Page do
       page.should be_valid
     end
     
-    it "should reject not existing page ids" do
+    it "should reject not existing page ids at save" do
       page = Page.new @page_attr.merge(:parent_id => @parent_page.id + 1)
-      page.should_not be_valid
+      page.save
     end
     
-    it "should reject itself as parent" do
+    it "should reject itself as parent at save" do
       @parent_page.parent_id @parent_page.id
-      @parent_page.should_not be_valid
+      @parent_page.save
     end
   end
   
   describe "position" do
+    it "should accept nil" do
+      page = Page.new @page_attr_minimal.merge(:position => nil)
+      page.should be_valid
+    end
     
+    it "should reject duplicates with pages with same parent at save" do
+      Page.create! @page_attr_minimal.merge(:position => 1)
+      Page.create! @page_attr_minimal.merge(:position => 1)
+    end
+    
+    it "should accept an integer greater than 0" do
+      page = Page.new @page_attr_minimal.merge(:position => 1)
+      page.should_not be_valid
+    end
+    
+    it "should reject values below 0" do
+      page = Page.new @page_attr_minimal.merge(:position => -23)
+      page.should_not be_valid
+    end
   end
 end
