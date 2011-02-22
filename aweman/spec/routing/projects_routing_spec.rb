880f250ae62806a73dd738bea0c1a127bc659b41
Dennis Boerm <allspark@planetcyborg.de>
2011-02-22 11:41:46 +0100

    routing spec

--- aweman/spec/routing/projects_routing_spec.rb
+++ aweman/spec/routing/projects_routing_spec.rb
@@ -31,5 +31,14 @@ describe ProjectsController do
       { :delete => "/projects/1" }.should route_to(:controller => "projects", :action => "destroy", :id => "1")
     end
 
+    describe "change group" do
+      it "recognizes and generates #add_group" do
+	{ :post => "/projects/1/add_group" }.should route_to(:controller => "projects", :action => "add_group", :id => "1")
+      end
+	
+      it "recognizes and generates #remove_group" do
+	{ :delete => "/projects/1/remove_group" }.should route_to(:controller => "projects", :action => "remove_group", :id => "1")
+      end
+    end
   end
 end
