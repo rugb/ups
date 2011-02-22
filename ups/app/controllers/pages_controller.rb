b9c145f45a171770e6668938d607addccdcff1a7
Dennis Boerm <allspark@planetcyborg.de>
2011-02-22 18:51:42 +0100

    pages controller scaffold

---
+++ ups/app/controllers/pages_controller.rb
@@ -0,0 +1,26 @@
+class PagesController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  def update
+  end
+
+  def home
+  end
+
+end
