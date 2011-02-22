0d460922ca300dcd64de805d623c36df7a3ff824
Dennis Boerm <allspark@planetcyborg.de>
2011-02-22 18:00:41 +0100

    language/page_content model

--- ups/app/models/page_content.rb
+++ ups/app/models/page_content.rb
@@ -1,2 +1,11 @@
 class PageContent < ActiveRecord::Base
+  attr_accessible :title, :text, :excerpt, :language_id
+  
+  validates :title, :presence => true, :length => { :maximum => 50 }
+  validates :page_id, :presence => true
+  validates :language_id, :presence => true
+  
+  belongs_to :page
+  belongs_to :language
+  
 end
