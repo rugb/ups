ec67b214c1f4c35a6dbde5ebe45a87f77016dba3
Dennis Boerm <allspark@planetcyborg.de>
2011-02-23 13:21:46 +0100

    category model/spec

--- ups/app/models/category.rb
+++ ups/app/models/category.rb
@@ -1,2 +1,13 @@
 class Category < ActiveRecord::Base
+  has_many :category_names
+  has_many :pages
+  
+  def initialize(options)
+    super()
+    
+    lang = Language.find_by_short(options[:language])
+    lang_id = lang.nil? ? 0 : lang.id
+    
+    self.category_names.build(:name => options[:name], :language_id => lang_id)
+  end
 end
--- ups/spec/models/category_spec.rb
+++ ups/spec/models/category_spec.rb
@@ -1,5 +1,41 @@
 require 'spec_helper'
 
 describe Category do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @attr = {
+      :name => "News",
+      :language => "en"
+    }
+  end
+  
+  it "should create a valid instance" do
+    Category.new(@attr)
+  end
+  
+  describe "validation" do
+    
+    it "should reject with NIL name" do
+      Category.new(@attr.merge(:name => NIL)).should_not be_valid
+    end
+    
+    it "should reject with empty name" do
+      Category.new(@attr.merge(:name => "")).should_not be_valid
+    end
+    
+    it "should reject with NIL language" do
+      Category.new(@attr.merge(:language => NIL)).should_not be_valid
+    end
+    
+    it "should reject with empty language" do
+      Category.new(@attr.merge(:language => "")).should_not be_valid
+    end
+    
+    it "should accept with long language" do
+      
+    end
+    
+    it "should accept with language id" do
+      
+    end
+  end
 end
