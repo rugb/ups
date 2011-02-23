df98318a756aa93ac63c5304fc3b166396a018fa
Philip Häusler <msquare@notrademark.de>
2011-02-23 20:01:59 +0100

    prepare routing for page_content

--- ups/app/helpers/application_helper.rb
+++ ups/app/helpers/application_helper.rb
@@ -1,6 +1,5 @@
 module ApplicationHelper
   def make_title
-   #(@title or @page_content.title or "ups") + " - university project system"
-   "temp"
+   (@title or (@page_content and @page_content.title) or "ups") + " - university project system"
   end
 end
--- ups/config/routes.rb
+++ ups/config/routes.rb
@@ -1,9 +1,17 @@
 Ups::Application.routes.draw do
   #resources :pages, :constraints => {:int_title => /[a-z_]{0,255}/}
   
-  match "/page/:id/:int_title" => "pages#show", :as => :show_page, :via => :get
+  match "/page/:id/:int_title(/:language_short)" => "pages#show", :as => :show_page, :via => :get
   
-  resources :pages
+  resources :pages do
+    member do
+      match 'new_content/:language_id' => "page_content#new", :via => :get
+      match 'create_content/:language_id' => "page_content#create", :via => :post
+      match 'edit_content/:language_id' => "page_content#edit", :via => :get
+      match 'update_content/:language_id' => "page_content#update", :via => :put
+      match 'destroy_content/:language_id' => "page_content#destroy", :via => :delete
+    end
+  end
   
   match "/credits" => "pages#credits"
   match "/setup" => "pages#setup"
