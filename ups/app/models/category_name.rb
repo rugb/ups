d1411fe5a2c07e7e8d18d17b82f8d09e271272e4
Dennis Boerm <allspark@planetcyborg.de>
2011-02-23 17:57:03 +0100

    name length validation and test

--- ups/app/models/category_name.rb
+++ ups/app/models/category_name.rb
@@ -2,6 +2,6 @@ class CategoryName < ActiveRecord::Base
   belongs_to :category
   belongs_to :language
   
-  validates :name, :presence => true, :uniqueness => true
+  validates :name, :presence => true, :uniqueness => true, :length => { :maximum => 255 }
   validates_numericality_of :language_id, :presence => true, :greater_than => 0
 end
