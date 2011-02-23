2e77239d5186e1468be1ce1771d27504f24deab4
Dennis Boerm <allspark@planetcyborg.de>
2011-02-23 15:13:36 +0100

    page tags

--- ups/app/models/tag.rb
+++ ups/app/models/tag.rb
@@ -1,2 +1,7 @@
 class Tag < ActiveRecord::Base
+  attr_accessible :name
+  
+  belongs_to :page
+  
+  validates :name, :presence => true, :uniqueness => { :case_sensitive => false }
 end
--- ups/spec/models/tag_spec.rb
+++ ups/spec/models/tag_spec.rb
@@ -1,5 +1,18 @@
 require 'spec_helper'
 
 describe Tag do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @attr = {
+      :name => "Test Tag"
+    }
+  end
+  
+  it "should create a valid instance" do
+    Tag.create!(@attr)
+  end
+  
+  it "should reject duplicates with case insensitive" do
+    Tag.create!(@attr)
+    Tag.new(@attr.merge(:name => @attr[:name].upcase)).should_not be_valid
+  end
 end
