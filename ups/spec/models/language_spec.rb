1c7029ac457e670bfe961eb77459c24e5264d506
Dennis Boerm <allspark@planetcyborg.de>
2011-02-23 14:33:20 +0100

    language

--- ups/spec/models/language_spec.rb
+++ ups/spec/models/language_spec.rb
@@ -3,8 +3,8 @@ require 'spec_helper'
 describe Language do
   before(:each) do 
     @attr = {
-      :short => "de",
-      :name => "deutsch"
+      :short => "es",
+      :name => "Spanisch"
     }
   end
   
@@ -13,13 +13,13 @@ describe Language do
   end
   
   it "should have a to_s method" do
-    lang = Language.new(:short => "de", :name => "Deutsch")
+    lang = Language.new(@attr)
     lang.should respond_to(:to_s)
   end
   
   it "should have a name (short) to_s output" do
-    lang = Language.new(:short => "de", :name => "Deutsch")
-    lang.to_s.should == "Deutsch (de)"
+    lang = Language.new(@attr)
+    lang.to_s.should == "#{@attr[:name]} (#{@attr[:short]})"
   end
   
   describe "validation" do
@@ -41,6 +41,11 @@ describe Language do
       it "should reject short longer than 2" do
         Language.new(@attr.merge(:short => "ddd")).should_not be_valid
       end
+      
+      it "should reject duplicates" do
+	Language.create!(@attr)
+	Language.new(@attr.merge(:name => "foo")).should_not be_valid
+      end
     end
     
     describe "name" do
@@ -57,6 +62,29 @@ describe Language do
         long_name = "a" * (255+1)
         Language.new(@attr.merge(:name => long_name)).should_not be_valid
       end
+      
+      it "should reject duplicates" do
+	Language.create!(@attr)
+	Language.new(@attr.merge(:short => "bl")).should_not be_valid
+      end
+    end
+  end
+  
+  describe "find_by_any" do
+    before(:each) do
+      @lang = Language.create!(@attr)
+    end
+    
+    it "should find by id" do
+      Language.find_by_any(@lang.id).should == @lang
+    end
+    
+    it "should find by short" do
+      Language.find_by_any(@lang.short).should == @lang
+    end
+    
+    it "should find by name" do
+      Language.find_by_any(@lang.name).should == @lang
     end
   end
 end
