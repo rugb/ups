5d72f1b955d1358f1d36ae961463d16979d62365
Philip Häusler <msquare@notrademark.de>
2011-02-24 21:07:35 +0100

    category new

--- ups/app/controllers/categories_controller.rb
+++ ups/app/controllers/categories_controller.rb
@@ -1,23 +1,27 @@
 class CategoriesController < ApplicationController
   def new
+    @edit_category = Category.new({})
+    @edit_category.category_names = Language.all.map do |language|
+      CategoryName.new(:language => language)
+    end
   end
-
+  
   def create
   end
-
+  
   def edit
   end
-
+  
   def update
   end
-
+  
   def destroy
   end
-
+  
   def show
   end
-
+  
   def index
   end
-
+  
 end
