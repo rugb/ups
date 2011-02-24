ff96a09dc52b625d26d8d54bab395a90688c8e6e
Dennis Boerm <allspark@planetcyborg.de>
2011-02-24 17:13:26 +0100

    users change, auth works

--- ups/config/authorization_rules.rb
+++ ups/config/authorization_rules.rb
@@ -1,19 +1,20 @@
 authorization do
   role :guest do
-    has_permission_on :users, :to => :create
+    has_permission_on :authorization_rules, :to => :read
+    has_permission_on :authorization_usages, :to => :read
   end
-  role :user do
-    includes :guest
-    has_permission_on :users, :to => :manage do
-      if_attribute :id => is {user.id}
-    end
-  end
+#   role :user do
+#     includes :guest
+#     has_permission_on :users, :to => :manage do
+#       if_attribute :id => is {user.id}
+#     end
+#   end
 end
-
-privileges do
-  privilege :manage, :includes => [:create, :read, :update, :delete]
-  privilege :read,   :includes => [:index, :show]
-  privilege :create, :includes => :new
-  privilege :update, :includes => :edit
-  privilege :delete, :includes => :destroy
-end
+ 
+# privileges do
+#   privilege :manage, :includes => [:create, :read, :update, :delete]
+#   privilege :read,   :includes => [:index, :show]
+#   privilege :create, :includes => :new
+#   privilege :update, :includes => :edit
+#   privilege :delete, :includes => :destroy
+#  end
