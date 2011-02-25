7516a9c9fe5d980f0a3dfdc10433cb35a13ac010
Philip Häusler <msquare@notrademark.de>
2011-02-25 17:11:34 +0100

    page content excerpts

--- ups/app/models/page_content.rb
+++ ups/app/models/page_content.rb
@@ -9,6 +9,7 @@ class PageContent < ActiveRecord::Base
   belongs_to :language
   
   before_save :update_int_title
+  before_save :update_excerpt
   
   def update_int_title
     if(page.int_title.blank? || Conf.get_default_language == language)
@@ -17,9 +18,18 @@ class PageContent < ActiveRecord::Base
     end
   end
   
+  def update_excerpt
+    self.excerpt = make_excerpt(text) if excerpt.blank?
+    p "update_excerpt", self, excerpt.blank?
+  end
+  
   private
   def make_short_title(title)
     title.tr(" ", "_").downcase.tr("^a-z0-9_", "")
+  end
+  
+  def make_excerpt(text)
+   (text.split(".")[0]+".")[0..255]
   end
   
 end
