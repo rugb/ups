907aa673463d9947981a1b48e72441d99725581b
Philip Häusler <msquare@notrademark.de>
2011-02-26 11:07:18 +0100

    settings done

--- ups/app/controllers/conf_controller.rb
+++ ups/app/controllers/conf_controller.rb
@@ -3,6 +3,11 @@ class ConfController < ApplicationController
   end
   
   def update
+    Conf.set_default_language(Language.find_by_any(params[:default_language]))
+    Conf.set_default_page(Page.find(params[:default_page]))
+    Conf.set_web_name(params[:web_name])
+    flash[:success] = "settings saved."
+    render :action => :index
   end
   
 end
