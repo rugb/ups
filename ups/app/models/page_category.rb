2c5c52c6509a9b1a6b2cd7524d8f89ca32f2d867
Philip Häusler <msquare@notrademark.de>
2011-02-26 15:12:01 +0100

    news creation

--- ups/app/models/page_category.rb
+++ ups/app/models/page_category.rb
@@ -1,4 +1,7 @@
 class PageCategory < ActiveRecord::Base
+  attr_accessor :checked, :category_attributes
+  
   belongs_to :page
   belongs_to :category
+  accepts_nested_attributes_for :category
 end
