5ab6b8f35fd1c6b90d5b185b37a1f6c7597fc1ff
Dennis Boerm <allspark@planetcyborg.de>
2011-03-01 13:32:17 +0100

    vote spec

--- ups/spec/models/vote_spec.rb
+++ ups/spec/models/vote_spec.rb
@@ -4,8 +4,20 @@ describe Vote do
   before(:each) do
     @user = User.new(:name => "test user", :email => "foo@bar.com", :openid => "openid")
     @attr = {
-      :user_id => @user.id
+      :ack => true
+#      :timeslot_id => @timeslot.id
       }
+  end
+
+  it "should create a valid instance" do
+    Vote.create!(@attr)
+  end
+
+  describe "validation" do
+    describe " of user_id" do
+      it "should reject empty user_id" do
+      end
+    end
   end
   
 end
