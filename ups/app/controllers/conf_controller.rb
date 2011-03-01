6eb688688ff3ae2fc3e41afe19471cce99ab0968
Philip Häusler <msquare@notrademark.de>
2011-03-01 16:59:06 +0100

    calendar view

--- ups/app/controllers/conf_controller.rb
+++ ups/app/controllers/conf_controller.rb
@@ -6,6 +6,7 @@ class ConfController < ApplicationController
     Conf.default_language = Language.find_by_any(params[:default_language])
     Conf.default_page = Page.find(params[:default_page])
     Conf.web_name = params[:web_name]
+    Conf.calendar = params[:calendar_url]
     flash[:success] = "settings saved."
     render :action => :index
   end
